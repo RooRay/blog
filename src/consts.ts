b55c38c2a811eaa0ba819384b116d11163b2175c
Ruaridh <86845749+RooRay@users.noreply.github.com>
2025-03-21 18:51:56 +0000

    Migrate old website-wide metadata

--- src/consts.ts
+++ src/consts.ts
@@ -1,40 +1,40 @@
 import type { Metadata, Site, Socials } from "@types";
 
 export const SITE: Site = {
-  TITLE: "Astro Micro",
-  DESCRIPTION: "Astro Micro is an accessible and lightweight blog.",
-  EMAIL: "trevortylerlee@gmail.com",
+  TITLE: "Ruaridh's Blog",
+  DESCRIPTION: "Welcome to my blog where I talk about tech and other things that interest me!",
+  EMAIL: "blog@rooray.xyz",
   NUM_POSTS_ON_HOMEPAGE: 5,
   NUM_PROJECTS_ON_HOMEPAGE: 3,
 };
 
 export const HOME: Metadata = {
   TITLE: "Home",
-  DESCRIPTION: "Astro Micro is an accessible theme for Astro.",
+  DESCRIPTION: "Welcome to my blog where I talk about tech and other things that interest me!",
 };
 
 export const BLOG: Metadata = {
   TITLE: "Blog",
-  DESCRIPTION: "A collection of articles on topics I am passionate about.",
+  DESCRIPTION: "The main blog featuring a collection of articles on topics I am passionate about.",
 };
 
 export const PROJECTS: Metadata = {
   TITLE: "Projects",
   DESCRIPTION:
-    "A collection of my projects with links to repositories and live demos.",
+    "A collection of my projects with links to their respective repositories.",
 };
 
 export const SOCIALS: Socials = [
   {
     NAME: "X (formerly Twitter)",
-    HREF: "https://twitter.com/boogerbuttcheek",
+    HREF: "https://twitter.com/Roo_Ray_",
   },
   {
     NAME: "GitHub",
-    HREF: "https://github.com/trevortylerlee",
+    HREF: "https://github.com/RooRay",
   },
   {
-    NAME: "Website",
-    HREF: "https://trevortylerlee.com",
+    NAME: "Discord",
+    HREF: "https://discord.com/users/347083401141944333/",
   },
 ];
